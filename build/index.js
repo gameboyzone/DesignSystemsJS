9b3a06014b3f4078192719276bd51f0dde500ee3
Hardik Shah <gameboyzone@users.noreply.github.com>
2023-12-03 02:03:50 -0600

    refactor(index): Removed unwanted comments

--- build/index.js
+++ build/index.js
@@ -1,22 +1,5 @@
 // import classes
 import { DesignSystems as DesignSystemsJS } from './classes/DesignSystems.js';
-// import data
-// import sampleDesignSystem from './data/SampleDesignSystem.json';
-/*
-let DesignSystems: DesignSystemsJS;
-
-// eslint-disable-next-line no-console
-console.log("--------------------------------------");
-console.log("Compiling DesignSystemJS ...");
-console.log("--------------------------------------");
-
-// Initialize DesignSystemsJS from a DesignSystem JSON object
-DesignSystems = new DesignSystemsJS([sampleDesignSystem]);
-
-console.log("--------------------------------------");
-console.log("DesignSystemJS compilation successful");
-console.log("--------------------------------------");
-*/
 // Leaving room to export multiple items
 export default DesignSystemsJS;
 //# sourceMappingURL=index.js.map
